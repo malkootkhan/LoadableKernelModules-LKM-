e4ea2675b598b8881f0c970cff93860fa0ec246b
Malkoot Khan <engr.mkhan1990@gmail.com>
2024-08-30 13:42:25 +0100

    description: Proper error handling is added; structured the code into a structure make it more reuseable and clear
    Signed-off-by: Malkoot Khan <engr.mkhan1990@gmail.com>

--- dummy_device.c
+++ dummy_device.c
@@ -4,6 +4,8 @@
 #include <linux/types.h>
 #include <linux/uaccess.h>
 #include <linux/cdev.h>
+#include <linux/slab.h>
+#include <linux/gfp_types.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Malkoot Khan");
@@ -19,8 +21,17 @@ MODULE_DESCRIPTION("character driver");
 #define MAX_BUFF_SIZE		(512)
 
 enum err_msg { SUCCESS, DEV_ALLOC_ERR, CLASS_CREAT_ERR, DEV_CREAT_ERR };
-
-typedef int status_t; 
+/*Writing these all variables into our own structure will make the code more reuseable and structured*/
+struct dummy_device
+{
+	dev_t dum_dev_numb;			/*device number*/
+	struct cdev dum_cdev;			/*struct to hold device*/
+	struct class *dum_dev_class;		/*device class it can be located at /dev/class/device*/	
+	struct file_operations dum_fops;	/*struct to hold device operations*/	
+	struct device *dum_device;		/*device struct */
+	char dum_dev_buff[MAX_BUFF_SIZE];	/*dummy device storage*/
+};
+struct dummy_device *dev_instance; /*global instance of dummy device */
 
 static int dummy_open(struct inode *, struct file *);
 static loff_t dummy_llseek(struct file *, loff_t, int);
@@ -28,14 +39,8 @@ static ssize_t dummy_read(struct file *, char __user *, size_t, loff_t *);
 static ssize_t dummy_write (struct file *, const char __user *, size_t, loff_t *);
 static long dummy_ioctl(struct file *, unsigned int, unsigned long);
 static int dummy_release(struct inode *, struct file *);
+static struct dummy_device* create_dummy_device(void);
 
-/*struct instances and variable required for a typical dummy character driver*/
-	dev_t devNo;				/*device number*/
-	struct cdev c_dev;			/*struct to hold device*/
-	struct class *dev_class;		/*device class it can be located at /dev/class/device*/	
-	struct file_operations fops;		/*struct to hold device operations*/	
-	struct device *my_device;		/*device struct */
-	char device_buff[MAX_BUFF_SIZE];	/*dummy device storage*/
 
 static int dummy_open(struct inode *, struct file *)
 {
@@ -53,7 +58,7 @@ static ssize_t dummy_read(struct file *filp, char __user *buff, size_t count, lo
 		count = MAX_BUFF_SIZE - *f_pos;
 	
 
-	if(copy_to_user(buff, &device_buff[*f_pos], count))
+	if(copy_to_user(buff, &dev_instance->dum_dev_buff[*f_pos], count))
 		return -EFAULT; /*bad address macro in errno-base.h*/
 
 	/*update the position value*/
@@ -76,7 +81,7 @@ static ssize_t dummy_write (struct file *filp, const char __user *buff, size_t c
 		return -ENOMEM; /*if the return is non-zero it means there is no space and the kernel has special macro for that  which is ENOMEM in errno-base.h*/ 
 	}
 	/*copy from user tokernel*/
-	if(copy_from_user(&device_buff[*f_pos], buff, count))
+	if(copy_from_user(&dev_instance->dum_dev_buff[*f_pos], buff, count))
 		return -EFAULT; /*bad address macro in errno-base.h*/
 	
 	/*update the position*/
@@ -144,8 +149,12 @@ static int dummy_release(struct inode *, struct file *)
 	printk("The device is closed\n");
 	return 0;
 }
+static struct dummy_device* create_dummy_device(void)
+{
+	struct dummy_device *dev;
+	dev = kmalloc(sizeof(struct dummy_device), GFP_KERNEL); /*GFP_KERNEL is a global kernel flag used for normal alloc where sleeping or blocking is allowed*/
 
-struct file_operations fops = {
+	struct file_operations fops = {
 		.owner = THIS_MODULE,
 		.open = dummy_open,
 		.read = dummy_read,
@@ -154,42 +163,57 @@ struct file_operations fops = {
 		.unlocked_ioctl = dummy_ioctl,
 		.release = dummy_release
 	};
+	dev->dum_fops = fops;
+	return dev;
+}
 
 static int __init dummy_driver_init(void){
 
-	status_t status;
+	int ret;
+	dev_instance = create_dummy_device();
 
 	/*1. alloc device number alloc_chrdev_region()*/
-	status = 0;
-	status = alloc_chrdev_region(&devNo, 0, 1, "dummy_device_name");
-	if(status != 0)
+	ret = alloc_chrdev_region(&dev_instance->dum_dev_numb, 0, 1, "dummy_device_name");
+	if(ret < 0)
 	{
 		printk("Device registration is not successful\n");
-		return DEV_ALLOC_ERR;
+		kfree(dev_instance);
+		return ret;
 	}
 
 	/*2. init device cdev_init()*/
-	cdev_init(&c_dev, &fops);
-	c_dev.owner = THIS_MODULE;
+	cdev_init(&dev_instance->dum_cdev, &dev_instance->dum_fops);
+	dev_instance->dum_cdev.owner = THIS_MODULE;
+
 	/*3. add device to the system: cdev_add()*/
-	status = 0;
-	status = cdev_add(&c_dev, devNo, 1);
-	if(status < 0){
+	ret = cdev_add(&dev_instance->dum_cdev, dev_instance->dum_dev_numb, 1);
+	if(ret < 0){
 		printk("device couldn't add to system\n");
-		return status;
+		unregister_chrdev_region(dev_instance->dum_dev_numb, 1); /*basically in if we are handling errors*/
+		kfree(dev_instance); /*it should be deallocated, to prevent memory leak */
+		return ret;
 	}
 
-	dev_class = class_create("dummy_class");
-	if(IS_ERR(dev_class))
+	dev_instance->dum_dev_class = class_create("dummy_class");
+	if(IS_ERR(dev_instance->dum_dev_class))
 	{
-		printk("device class couldn't create");
-		return CLASS_CREAT_ERR;
+
+		printk("device class couldn't create\n");
+		cdev_del(&dev_instance->dum_cdev);				/*Follow the sequence of reversing the operations, in case of failure*/
+		unregister_chrdev_region(dev_instance->dum_dev_numb, 1);	/*basically in if we are handling errors*/
+		kfree(dev_instance);						/*it should be deallocated, to prevent memory leak */
+		return PTR_ERR(dev_instance->dum_dev_class);			/*error code is converted to pointer and return as type is pointer*/
+										/*macros like IS_ERR, ERR_PTR, and PTR_ERR used to convert error to pointer and pointer to error code*/
 	}
 
-	my_device = device_create(dev_class, NULL, devNo, NULL, "dummy_device");
-	if(IS_ERR(my_device)){
+	dev_instance->dum_device = device_create(dev_instance->dum_dev_class, NULL, dev_instance->dum_dev_numb, NULL, "dummy_device");
+	if(IS_ERR(dev_instance->dum_device)){
 		printk("device creation failed\n");
-		return DEV_CREAT_ERR;
+		class_destroy(dev_instance->dum_dev_class);
+		cdev_del(&dev_instance->dum_cdev);
+		unregister_chrdev_region(dev_instance->dum_dev_numb, 1);	/*device number and device count*/
+		kfree(dev_instance);
+		return PTR_ERR(dev_instance->dum_device);			/*converts pointer to its relevant error code which long int*/
 	}
 	
 	printk("device is successfully integrated into the kernel\n");
@@ -199,10 +223,11 @@ static int __init dummy_driver_init(void){
 
 static void __exit dummy_driver_exit(void){
 	
-	device_destroy(dev_class, devNo);
-	class_destroy(dev_class);
-	cdev_del(&c_dev);
-	unregister_chrdev_region(devNo/*firstDevNo*/, 0/*device cout*/);
+	device_destroy(dev_instance->dum_dev_class,dev_instance->dum_dev_numb);
+	class_destroy(dev_instance->dum_dev_class);
+	cdev_del(&dev_instance->dum_cdev);
+	unregister_chrdev_region(dev_instance->dum_dev_numb/*firstDevNo*/, 0/*device cout*/);
+	kfree(dev_instance);
 	printk("The device is successfully unloaded!\n");
 
 }
