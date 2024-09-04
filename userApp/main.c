0cfe86777a129bac568d4061d68720c34335e80c
Malkoot Khan <engr.mkhan1990@gmail.com>
2024-09-04 19:08:11 +0100

    updates: added simple open and read system calls to interact with our dummy driver
    Signed-off-by: Malkoot Khan <engr.mkhan1990@gmail.com>

--- userApp/main.c
+++ userApp/main.c
@@ -3,35 +3,57 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef enum {SUCCESS, FAILURE}err_code;
 
+#define  READ_MODE      0
+#define  WRITE_MODE     1
+#define RWR_MODE        2
+
+
+void help()
+{
+      perror("Invalid argument e.g:  <exceutableFile> arg1");
+      printf("arg1: file path\n");
+
+}
+
 int main(int argc, char *argv[])
 {
-   if(argc != 2){
-      perror("Invalid argument e.g:  <exceutableFile> arg1");
+
+   if(strcmp(argv[1],"help") == 0) {
+      help();
+      return SUCCESS;
+   }
+
+   if(argc != 3){
+      help();
       return -FAILURE;
    }
-
-   int count = atoi(argv[1]); /*10 bye should be read*/
-   char buff[11];
-   int fd = open("/dev/dummy_device_1", O_RDONLY);
-   if(fd < 0){
-      perror("open");
-      goto failed;
+   int byteCount = atoi(argv[2]);
+   char buff[100];
+   char file_path[24];
+   strcpy(file_path, argv[1]);
+   int fd = open(file_path, O_RDONLY);
+   if(fd < 0)
+   {
+      perror("open: ");
+      goto out;
    }
-   printf("File open successful!\n");
-
-   int ret = read(fd, buff, 10);
-   if(ret < 0){
-      perror("read");
-      goto failed;
+   printf("open successful\n");
+   int ret = read(fd, buff, byteCount);
+   if(ret < 0)
+   {
+      perror("read: ");
+      goto out;
    }
+   printf("read successful\n");
+   printf("read data: %s\n", buff);
 
-   printf("readData: %s\n", buff);
-   
-failed:
-   close(fd);
 
     return SUCCESS;
+out:
+    close(fd);
+    return -FAILURE;
 }
