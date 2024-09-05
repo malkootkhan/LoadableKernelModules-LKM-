a1d63a28748f46788ffba90bbefed5652e966887
Malkoot Khan <engr.mkhan1990@gmail.com>
2024-09-05 13:37:22 +0100

    updates: Step:1=> two device instances are create and just regitered and unregistered
    Signed-off-by: Malkoot Khan <engr.mkhan1990@gmail.com>

--- dummy_platform_dev.c
+++ dummy_platform_dev.c
@@ -1,32 +1,42 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/fs.h>
-#include <linux/types.h>
-#include <linux/uaccess.h>
-#include <linux/cdev.h>
-#include <linux/slab.h>
-#include <linux/gfp_types.h>
+#include <linux/platform_device.h> /*this struct holds the device register/unregister operations and struct platform_device*/
 
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Malkoot Khan");
-MODULE_DESCRIPTION("dummy platform device");
+/*1. The first and most important thing we do here: create platform device using struct paltform_device*/
 
+/*let create two devices*/
+/*there are more members but for now we initize only these two, id shows an index somehow for platform devic*/
+struct platform_device dummy_platform_dev_1 = {
+  .name = "my_dummy_platform_device",
+  .id = 0
+};
+struct platform_device dummy_platform_dev_2 = {
+  .name = "my_device_platform_device",
+  .id = 1
+};
+/*as usual we have to register these devices*/
 
 static int __init dummy_platform_dev_init(void)
 {
+  /*Register platform devices => later should be unregistered in exit function*/
+  platform_device_register(&dummy_platform_dev_1);
+  platform_device_register(&dummy_platform_dev_2);
 
-  pr_info("dummy platform device is inserted to kernel successfully\n");
 
   return 0;
 }
 
 static void __exit dummy_platform_dev_exit(void)
 {
-  pr_info("dummy platform device is removed from kernel successfully\n");
+  platform_device_unregister(&dummy_platform_dev_1);
+  platform_device_unregister(&dummy_platform_dev_2);
 }
 
 
 module_init(dummy_platform_dev_init);
 module_exit(dummy_platform_dev_exit);
 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Malkoot Khan");
+MODULE_DESCRIPTION("dummy platform device");
